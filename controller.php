0cdbc0bebed8deaa046bd3c00b4bd6485bfd061a
alexander <alexander198961@gmail.com>
2016-08-26 22:41:46 -0400

    fix

--- controller.php
+++ controller.php
@@ -1,43 +1,36 @@
 <?php
 
-$folder = "/files/";
-$file = $folder . $_GET['file'];
+$folder = '/files/';
+$file = $folder.$_GET['file'];
 
-if (!file_exists($file) && $_GET['file'] != "")
-	{
-	echo "File doesn't exists  $filen";
-	exit(1);
-	}
+if (!file_exists($file) && $_GET['file'] != '') {
+    echo "File doesn't exists  $filen";
+    exit(1);
+}
 
-switch ($_SERVER['REQUEST_METHOD'])
-	{
-case "DELETE":
-	unlink($file);
-	echo "File was removed";
-	break;
+switch ($_SERVER['REQUEST_METHOD']) {
+case 'DELETE':
+    unlink($file);
+    echo 'File was removed';
+    break;
 
-case "GET":
-	header('Content-Description: File Transfer');
-	header('Content-Type: application/octet-stream');
-	header('Content-Disposition: attachment; filename=' . basename($file));
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate');
-	header('Pragma: public');
-	header('Content-Length: ' . filesize($file));
-	readfile($file);
-	break;
+case 'GET':
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: '.filesize($file));
+    readfile($file);
+    break;
 
-case "POST":
-	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $folder . $_FILES['userfile']['name']))
-		{
-		print "File  was  uploaded.";
-		}
-	  else
-		{
-		print "Error during upload!";
-		}
+case 'POST':
+    if (move_uploaded_file($_FILES['userfile']['tmp_name'], $folder.$_FILES['userfile']['name'])) {
+        echo 'File  was  uploaded.';
+    } else {
+          echo 'Error during upload!';
+      }
 
-	break;
-	}
-
-?>
+    break;
+    }
